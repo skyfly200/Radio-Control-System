49af67d410ab547c64197e7db54003d291e6a920
Skyler Fly-Wilson <skyfly200@gmail.com>
2016-09-22 13:08:40 -0600

    Event Mgmt. + more
    Adds functions for adding and deleting events in the database
    adds a form to make new events (incomplete implementation)

--- db/events-db.js
+++ db/events-db.js
@@ -1,42 +1,64 @@
 const sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./db/events');
+var db;
+
+// open the database
+exports.open = function () {
+  db = new sqlite3.Database('./db/events');
+}
+
+// close the database
+exports.close = function () {
+  db.close();
+}
 
 // register a new event
-exports.register = function (title) {
+exports.register = function (title, type, date, time, priority, file, notes) {
   db.serialize(function() {
     // use a prepared statement for insert
     var stmt = db.prepare('INSERT INTO events VALUES (?,?,?,?,?,?,?)');
     stmt.run(title, type, date, time, priority, file, notes);
     stmt.finalize();
   });
-  db.close();
 }
 
 // delete an event
-exports.delete = function (title, type, date, time, priority, file, notes) {
+exports.delete = function (title) {
   db.serialize(function() {
-    // use a prepared statement for insert
-    var stmt = db.prepare('DELETE FROM events WHERE title=(?)');
+    // use a prepared statement for delete
+    var stmt = db.prepare('DELETE FROM events WHERE title = ?');
     stmt.run(title);
     stmt.finalize();
   });
-  db.close();
 }
 
-// print all events (for debugging)
+
+// find and return an event
 exports.find = function (title) {
-  // print all events
-  db.each('SELECT * FROM events WHERE', function(err, row) {
-    if (err) {console.log}
-    console.log(row.id + ': ' + row.title + ' ' + row.type + ' ' + row.date + ' ' + row.time + ' ' + row.file);
+  db.serialize(function() {
+    // print all events
+    db.each('SELECT * FROM events WHERE title = ?', title, function(err, row) { // fix with statment
+      if (err) {console.log(err);}
+      console.log(row.title + ' ' + row.type + ' ' + row.date + ' ' + row.time + ' ' + row.file);
+    });
   });
 }
 
 // print all events (for debugging)
 exports.all = function () {
-  // print all events
-  db.each('SELECT * rowid AS id FROM events', function(err, row) {
-    if (err) {console.log}
-    console.log(row.id + ': ' + row.title + ' ' + row.type + ' ' + row.date + ' ' + row.time + ' ' + row.file);
+  db.serialize(function() {
+    // print all events
+    db.each('SELECT * FROM events', function(err, row) {
+      if (err) {console.log(err);}
+      console.log(row.title + '/' + row.type + ' - ' + row.date + ' ' + row.time + ' - ' + row.file);
+      if (row.notes != '') { console.log('Notes: ' + row.notes) } // print any notes on separate line
+    });
+  });
+}
+
+// delete all events  BE CAREFUL! this function DELETEs all events!!!
+exports.deleteAll = function (title) {
+  db.serialize(function() {
+    // delete all events
+    db.run('DELETE FROM events');
   });
 }
--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const express = require('express');
 var app = express();
 const events = require('./db/events-db');
 
+// test db functions
+events.open();
+events.register('Test Show', 'show', '12/31/99', '00:00-24:00', '0', 'test.m3u', 'test');
+events.all();
+events.delete('Test Show');
+events.close();
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -15,7 +21,10 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(express.static('public'));
 
 // Compile PUG templates
-fs.writeFile("public/form.htm", pug.renderFile("public/form.pug", {}),  function(err) {
+fs.writeFile("public/clever-cmd.htm", pug.renderFile("public/clever-cmd.pug", {}),  function(err) {
+   if (err) { return console.error(err); }
+});
+fs.writeFile("public/add-event.htm", pug.renderFile("public/add-event.pug", {}),  function(err) {
    if (err) { return console.error(err); }
 });
 
@@ -25,9 +34,10 @@ app.get('/', function (req, res) {
   res.send('Put Radio Control System Api Docs Here!');
 })
 
+// <- Clever Interface ->
 // get clever command form
 app.get('/form', function (req, res) {
-  res.sendFile( __dirname + "/public/form.htm" );
+  res.sendFile( __dirname + "/public/clever-cmd.htm" );
 })
 
 // get clever command
@@ -36,6 +46,20 @@ app.get('/clever', function (req, res) {
   var arg = req.query.arg;
   res.end(clever.cleverCmd(cmd, arg));
 })
+
+// <- Events Interface ->
+// get new event form
+app.get('/add-event', function (req, res) {
+  res.sendFile( __dirname + "/public/add-event.htm" );
+})
+
+// process post request to add a new event
+app.post('/new-event', function (req, res) {
+  var cmd = req.query.command;
+  var arg = req.query.arg;
+  res.end(clever.cleverCmd(cmd, arg));
+})
+
 
 // <<<- Start the Express App ->>>
 var server = app.listen(8081, function () {

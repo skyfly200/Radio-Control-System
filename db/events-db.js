0a4c237c17933f496d012d5890d9c6870392c3e8
Skyler Fly-Wilson <skyfly200@gmail.com>
2016-09-25 17:49:57 -0600

    DB Functions + File Upload
    Added update, find, delete, and create interfaces to events database
    Added file upload form and handler

--- db/events-db.js
+++ db/events-db.js
@@ -21,34 +21,153 @@ exports.register = function (title, type, date, time, priority, file, notes) {
   });
 }
 
-// delete an event
-exports.delete = function (title) {
-  db.serialize(function() {
-    // use a prepared statement for delete
-    var stmt = db.prepare('DELETE FROM events WHERE title = ?');
-    stmt.run(title);
-    stmt.finalize();
-  });
-}
-
-
-// find and return an event
-exports.find = function (title, callback) {
+// delete an event with matching parameters
+// find events by matching parameters
+// names = array of parameter names to match
+// values = array of values to mach, in order with corisponding names array
+// callback = function to pass result to
+exports.delete = function (names, values, callback) {
   // intialize callback function if undefined
   if (typeof callback != "function") {
-    callback = function() {}
+    callback = function(result) {}
   }
 
+  // build the WHERE parameters string based on available parameters
+  var validParams = ['title', 'type', 'date', 'time', 'priority', 'file'];
+  var parameters = '';
+  for (n in values) {
+    if (validParams.indexOf(names[n]) != -1) {
+      parameters += ( names[n] + "=?");
+      if ( n < (values.length - 1) ) { parameters += ' AND '; }
+    }
+    else{
+      console.error('Invalid Parameter Name: ' + names[n]);
+      callback (false);
+      return;
+    }
+  }
+
+  // build SQL query
+  var query = 'DELETE FROM events WHERE ' + parameters;
+
+  // run db commands in series
   db.serialize(function() {
-    // print all events
-    db.each('SELECT * FROM events WHERE title = ?', title, function(err, row) { // fix with statment
-      if (err) {console.error(err);}
-      callback (row);
+    // delete all events
+    db.run(query, values, function(err) {
+      if (err === null) {
+        callback (true);
+      }else {
+        console.error(err);
+        callback (false);
+      }
     });
   });
 }
 
-// return all events
+// Update events with matching parameters
+// names = array of parameter names to match
+// values = array of values to mach, in order with corisponding names array
+// callback = function to pass modified events
+exports.update = function (names, values, updateNames, newValues, callback) {
+  // intialize callback function if undefined
+  if (typeof callback != "function") {
+    callback = function(events) {}
+  }
+
+  // build the WHERE parameters string based on available parameters
+  var validParams = ['title', 'type', 'date', 'time', 'priority', 'file', 'notes'];
+  var parameters = '';
+  for (n in values) {
+    if (validParams.indexOf(names[n]) != -1) {
+      parameters += ( names[n] + "=?");
+      if ( n < (values.length - 1) ) { parameters += ' AND '; }
+    }
+    else{
+      console.error('Invalid Parameter Name: ' + names[n]);
+      callback (false);
+      return;
+    }
+  }
+
+  // build the SET parameters string based on available parameters
+  var validParams = ['title', 'type', 'date', 'time', 'priority', 'file', 'notes'];
+  var setParams = '';
+  for (n in newValues) {
+    if (validParams.indexOf(updateNames[n]) != -1) {
+      setParams += ( updateNames[n] + "=?");
+      if ( n < (newValues.length - 1) ) { setParams += ', '; }
+    }
+    else{
+      console.error('Invalid Parameter Name: ' + updateNames[n]);
+      callback (false);
+      return;
+    }
+  }
+
+  // build SQL update and query staments
+  // (CHANGE TO USING PREPARED STATEMENTS!!!!!!)
+  var update = 'UPDATE events SET ' + setParams + ' WHERE ' + parameters;
+  console.log(update);
+
+  var updateValues = newValues.concat(values);
+
+  // run db commands in series
+  db.serialize(function() {
+    // update all matching
+    db.all(update, updateValues, function(err, rows) {
+      if (err) {
+        console.error(err);
+        callback (false);
+      }else {
+        callback (true);
+      }
+    });
+  });
+}
+
+// find events by matching parameters
+// names = array of parameter names to match
+// values = array of values to mach, in order with corisponding names array
+// callback = function to pass events found to
+exports.find = function (names, values, callback) {
+  // intialize callback function if undefined
+  if (typeof callback != "function") {
+    callback = function(events) {}
+  }
+
+  // build the WHERE parameters string based on available parameters
+  var validParams = ['title', 'type', 'date', 'time', 'priority', 'file'];
+  var parameters = '';
+  for (n in values) {
+    if (validParams.indexOf(names[n]) != -1) {
+      parameters += ( names[n] + "=?");
+      if ( n < (values.length - 1) ) { parameters += ' AND '; }
+    }
+    else{
+      console.error('Invalid Parameter Name: ' + names[n]);
+      callback (false);
+      return;
+    }
+  }
+
+  // build SQL query
+  var query = 'SELECT * FROM events WHERE ' + parameters;
+
+  // run db commands in series
+  db.serialize(function() {
+    // print all events
+    db.all(query, values, function(err, rows) {
+      if (err) {
+        console.error(err);
+        callback (false);
+      }else {
+        callback (rows);
+      }
+    });
+  });
+}
+
+// get all events
 exports.all = function (callback) {
   // intialize callback function if undefined
   if (typeof callback != "function") {
--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 const clever = require('./clever/clever');
 const pug = require('pug');
-const fs =  require("fs")
+const fs = require('fs-extra');
 const url = require('url');
 const bodyParser = require('body-parser');
 const express = require('express');
+var formidable = require('formidable');
+var util = require('util');
 var app = express();
 const events = require('./db/events-db');
 
-// test db functions
-//events.open();
-//events.register('Test Show', 'show', '12/31/99', '00:00-24:00', '0', 'test.m3u', 'test');
-//events.deleteAll();
-//events.printAll();
-//console.log("All Events:");
-//events.all( function(events) {
-//  console.log(events);
-//});
-//events.delete('Test Show');
-//events.close();
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -34,6 +25,38 @@ app.get('/', function (req, res) {
   res.send('Put Radio Control System Api Docs Here!');
 });
 
+// <- File Upload ->
+// file upload form
+app.get('/file-upload', function (req, res) {
+  res.render( "file-upload" );
+});
+
+// file upload handler
+app.post('/upload', function(req, res) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.end('received upload!');
+  });
+
+  form.on('end', function(fields, files) {
+    /* Temporary location of our uploaded file */
+    var temp_path = this.openedFiles[0].path;
+    /* The file name of the uploaded file */
+    var file_name = this.openedFiles[0].name;
+    /* Location where we want to copy the uploaded file */
+    var new_location = './content/';
+
+    fs.copy(temp_path, new_location + file_name, function(err) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("success!")
+      }
+    });
+  });
+});
+
 // <- Clever Interface ->
 // render clever command form
 app.get('/clever-cmd', function (req, res) {
@@ -43,7 +66,7 @@ app.get('/clever-cmd', function (req, res) {
 // get clever command
 app.get('/clever', function (req, res) {
   clever.cleverCmd(req.query.command, req.query.arg, function(result) {
-    res.end(result);
+    res.end(JSON.stringify(result));
   });
 });
 
@@ -54,22 +77,120 @@ app.get('/add-event', function (req, res) {
 });
 
 // process post request to add a new event
-app.post('/new-event', urlencodedParser, function (req, res) {
-  var p = req.body; // get parameters
+app.post('/post-event', urlencodedParser, function (req, res) {
+  var p = req.body; // get POST parameters
   events.open();
   events.register(p.title, p.type, p.date, p.time, p.priority, p.file, p.notes);
   res.end('registered event');
   events.close();
 });
 
-// return a list of json encoded events
-app.get('/events', function(req, res) {
+
+// respond with a json encoded list of all events
+app.get('/get-events', function(req, res) {
   events.open();
   events.all( function(result) {
-    for (event in result) {
-      res.write(event + ': ' + result[event].title + '\n');
+    res.end(JSON.stringify(result));
+    events.close();
+  });
+});
+
+// respond with a json encoded list of all events with matching values
+app.get('/get-event', function(req, res) {
+  // arrays of parameters to match against
+  var names = [];
+  var values = [];
+
+  var p = req.query; // set p to GET parameters
+  // arrays of all posible parameters and their set values
+  var paramNames = ['title', 'type', 'date', 'time', 'priority', 'file', 'notes'];
+  var params = [p.title, p.type, p.date, p.time, p.priority, p.file, p.notes];
+
+  // determine what parameters are defined and append them to matching arrays
+  for (p in params) {
+    if (params[p] != undefined && params[p] != '') {
+      names.push(paramNames[p]);
+      values.push(params[p]);
     }
-    res.end();
+  }
+
+  // open the db and execute the querry on the database
+  events.open();
+  events.find(names, values, function(result) {
+    // return the results as json and close the db
+    res.end(JSON.stringify(result));
+    events.close();
+  });
+});
+
+// update all events with matching values
+app.post('/update-event', urlencodedParser, function(req, res) {
+  // arrays of parameters to match against
+  var names = [];
+  var values = [];
+  // arrays of parameters to update
+  var updateNames = [];
+  var updateValues = [];
+
+  var p = req.body; // set p to POST parameters
+  // arrays of all parameter names and values set for them
+  var paramNames = ['title', 'type', 'date', 'time', 'priority', 'file'];
+  var params = [p.title, p.type, p.date, p.time, p.priority, p.file];
+  var newParams = [p.title_u, p.type_u, p.date_u, p.time_u, p.priority_u, p.file_u];
+
+  // determine what parameters are defined and append them to matching arrays
+  for (p in params) {
+    if (params[p] != undefined) {
+      names.push(paramNames[p]);
+      values.push(params[p]);
+    }
+  }
+  // determine what update parameters are defined and append them to update arrays
+  for (p in newParams) {
+    if (newParams[p] != undefined) {
+      updateNames.push(paramNames[p]);
+      updateValues.push(newParams[p]);
+    }
+  }
+
+  // open the db and execute the querry on the database
+  events.open();
+  events.update(names, values, updateNames, updateValues, function(result) {
+    // return result of delete and close db
+    var output = JSON.stringify(result);
+    if (output != '' || output != undefined ) { res.end(output); }
+    else { res.end(false); }
+    events.close();
+  });
+});
+
+
+// delete all events with matching values
+app.post('/delete-event', urlencodedParser, function(req, res) {
+  // arrays of parameters to match against
+  var names = [];
+  var values = [];
+
+  var p = req.body; // set p to POST parameters
+  // arrays of all parameter names and values set for them
+  var paramNames = ['title', 'type', 'date', 'time', 'priority', 'file'];
+  var params = [p.title, p.type, p.date, p.time, p.priority, p.file];
+
+  // determine what parameters are defined and append them to matching arrays
+  for (p in params) {
+    if (params[p] != undefined) {
+      names.push(paramNames[p]);
+      values.push(params[p]);
+    }
+  }
+
+  // open the db and execute the querry on the database
+  events.open();
+  events.delete(names, values, function(result) {
+    // return result of delete and close db
+    var output = JSON.stringify(result);
+    if (output != '' || output != undefined ) { res.end(output); }
+    else { res.end(false); }
     events.close();
   });
 });
